c041e8cc5acf238a86f8cccdcba07c33ed3a1f54
Diego Lopez <diegolopezuncc@gmail.com>
2024-07-24 04:32:22 -0400

    chore: updated file

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import Extension, setup
-
-module = Extension('my_extension', sources=['my_extension.c'])
-
-setup(
-  name='MyCustomDataStructure',
-  version='0.1.0',
-  ext_modules=[module]
-)
